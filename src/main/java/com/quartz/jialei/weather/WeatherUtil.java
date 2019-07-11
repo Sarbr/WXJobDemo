7e6acad0bd8e85a4754b0be6740aeb6b8ff4e21b
Sarbr <715125467@qq.com>
2019-07-11 09:26:08 +0800

    输出的样式调整

--- src/main/java/com/quartz/jialei/weather/WeatherUtil.java
+++ src/main/java/com/quartz/jialei/weather/WeatherUtil.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Slf4j
 @SuppressWarnings("Duplicates")
 public class WeatherUtil {
-
+    private static String[] str = new String[]{"紫外线指数", "运动指数", "血糖指数", "穿衣指数", "洗车指数", "空气污染扩散指数"};
     /**
      * @author : sarbr
      * @Description :  国外的接口  http://api.apixu.com/v1/forecast.json?key=d41eb19c8e944d8ca5054053191506&q=haidian
@@ -20,18 +20,21 @@ public class WeatherUtil {
      */
     public static String getWeather(String cityName) {
         StringBuilder sb=new StringBuilder();
+        StringBuilder sb2=new StringBuilder();
         try {
             HttpResponse execute = HttpUtil.createPost("https://www.tianqiapi.com/api/?version=v1&&cityid=".concat(cityName)).contentType("application/json").timeout(HttpRequest.TIMEOUT_DEFAULT).execute();
             String body = execute.body();
             Weather weather = JSONArray.parseObject(body,Weather.class);
             Weather.DataBean dataBean = weather.getData().get(0);
             List<Weather.DataBean.IndexBean> index = dataBean.getIndex();
-            StringBuilder sb2=new StringBuilder();
-            index.forEach(i->sb2.append("### <font color=\"warning\">").append(i.getTitle().replaceAll("<em>", "").replaceAll("</em>", "")).append(":").append(i.getDesc()).append("</font>").append("\n"));
-            sb.append("# ").append("<font color=\"info\">").append(weather.getCity().concat(":")).append(dataBean.getWeek()).append(",").append(dataBean.getWea()).append("</font> \n")
-                    .append("## ").append("<font color=\"comment\">".concat(weather.getUpdate_time())).append("</font> \n")
-                    .append("## ").append("<font color=\"comment\">").append(dataBean.getTem2().concat("~").concat(dataBean.getTem1())).append("</font> \n")
-                    .append(sb2.toString());
+            for (int i = 0; i < index.size(); ++i) {
+                Weather.DataBean.IndexBean indexBean = index.get(i);
+                sb2.append("### <font color=\"warning\">").append(str[i]).append(":").append(indexBean.getDesc()).append("</font>").append("\n");
+            }
+            sb.append("# ").append("<font color=\"info\">").append(weather.getCity().concat(":")).append(dataBean.getWeek()).append(",").append(dataBean.getWea()).append("</font> \n").append
+                    ("## ").append("<font color=\"comment\">".concat(weather.getUpdate_time())).append("</font> \n").append
+                    ("## ").append("<font color=\"comment\">").append(dataBean.getTem2().concat("~").concat(dataBean.getTem1())).append("</font> \n").append
+                    (sb2.toString());
         } catch (Exception e) {
             log.error("加载文件出错"+e);
         }
